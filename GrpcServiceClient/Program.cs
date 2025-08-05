aabe75e6d01d301dbf32ea7e6ced81f6e80a1d68
Fernando Lima <fernando@umavida.com>
2025-08-05 09:37:33 -0300

    Add project files.

---
+++ GrpcServiceClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7246");
+var client = new Greeter.GreeterClient(channel);
+
+var myTasks = new List<Task>();
+var stopwatch = Stopwatch.StartNew();
+for (int i = 0; i < 10000; i++)
+{
+    myTasks.Add(sayHello(client, i));
+}
+Task.WaitAll(myTasks);
+
+stopwatch.Stop();
+Console.WriteLine($"Tempo gasto para execução do for: {stopwatch.Elapsed}");
+//Tempo gasto para execução do for: 00:01:03.4855090
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+static async Task sayHello(Greeter.GreeterClient client, int i)
+{
+    var reply = await client.SayHelloAsync(
+        new HelloRequest { Name = $"GreeterClient::{i}" });
+    Console.WriteLine("Server response: " + reply.Message);
+}
---
+++ RestServiceClient/Program.cs
@@ -0,0 +1,24 @@
+﻿// Chamada HTTP para https://localhost:7262/weatherforecast
+using System.Diagnostics;
+
+using var httpClient = new HttpClient();
+
+var myTasks = new List<Task>();
+var stopwatch = Stopwatch.StartNew();
+for (int i = 0; i < 100000; i++)
+{
+    myTasks.Add(RequestRest(httpClient, i));
+}
+Task.WaitAll(myTasks);
+
+stopwatch.Stop();
+Console.WriteLine($"Tempo gasto para execução do for: {stopwatch.Elapsed}");
+
+static async Task RequestRest(HttpClient httpClient, int i)
+{
+    var response = await httpClient.GetAsync("https://localhost:7262/weatherforecast");
+    response.EnsureSuccessStatusCode();
+    var content = await response.Content.ReadAsStringAsync();
+    Console.WriteLine($"Resposta da API weatherforecast:{i}");
+}
+//Tempo gasto para execução do for: 00:00:03.8973082
